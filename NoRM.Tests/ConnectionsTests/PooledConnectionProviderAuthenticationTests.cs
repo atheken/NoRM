d51fdf0b4cb2a2a6068b670c921fd98b5f41289f
Karl Seguin <karl@openmymind.net>
2010-02-26 17:11:52 -0500

    test for pooled connection provider, + fix to provider itself

---
+++ NoRM.Tests/ConnectionsTests/PooledConnectionProviderAuthenticationTests.cs
@@ -0,0 +1,27 @@
+namespace NoRM.Tests
+{
+    using Xunit;
+
+    public class PooledConnectionProviderAuthenticationTests : AuthenticatedFixture
+    {
+        [Fact]
+        public void ThrowsExceptionIfConnectingWithInvalidCredentials()
+        {
+            var provider = new PooledConnectionProvider(ConnectionStringBuilder.Create(AuthenticatedConnectionString("bad", "boy")));
+            var ex = Assert.Throws<MongoException>(() => provider.Open(null));
+            Assert.Equal("Authentication Failed", ex.Message);
+        }
+
+
+        //won't pass on some 1.3.x builds of the server, but will pass against newest, or stable (1.2.3).
+        [Fact]
+        public void AuthenticatesWithProperCredentials()
+        {
+            var provider = new PooledConnectionProvider(ConnectionStringBuilder.Create(AuthenticatedConnectionString("usr", "8e156e298e19afdc3a104ddd172a2bee")));
+            var connection = provider.Open(null);
+            Assert.Equal(true, connection.Client.Connected);
+            provider.Close(connection);           
+            
+        }
+    }
+}
--- NoRM/Connections/PooledConnectionProvider.cs
+++ NoRM/Connections/PooledConnectionProvider.cs
@@ -6,13 +6,11 @@ namespace NoRM
 
     //todo: review (thrown together quickly)
     internal class PooledConnectionProvider : ConnectionProvider
-    {
-        private const int MAXIMUM_POOL_SIZE = 5; //todo: make configurable via connection string
-        private const int TIMEOUT = 15000; //todo: make configurable via connection string
-
-        private readonly ConnectionStringBuilder _builder;
+    {        
+        private readonly ConnectionStringBuilder _builder;        
         private readonly Queue<IConnection> _idlePool;
         private readonly Semaphore _tracker; //todo: semaphore a little heavy?
+        private readonly int _timeout;
         
         public override ConnectionStringBuilder ConnectionString
         {
@@ -22,21 +20,26 @@ namespace NoRM
         public PooledConnectionProvider(ConnectionStringBuilder builder)
         {
             _builder = builder;
-            _idlePool = new Queue<IConnection>(MAXIMUM_POOL_SIZE);
-            _tracker = new Semaphore(0, MAXIMUM_POOL_SIZE);
-            for(var i = 0; i < MAXIMUM_POOL_SIZE; ++i)
-            {
-                EnqueueIdle(CreateNewConnection());
-            }            
+            _idlePool = new Queue<IConnection>(builder.PoolSize);
+            _tracker = new Semaphore(builder.PoolSize, builder.PoolSize);
+            _timeout = builder.Timeout*1000;          
         }
 
         public override IConnection Open(string options)
         {
-            if (!_tracker.WaitOne(TIMEOUT))
+            if (!_tracker.WaitOne(_timeout))
             {
-                throw new TimeoutException();
+                throw new MongoException("Connection timeout trying to get connection from connection pool");
             }
-            var connection = _idlePool.Dequeue();
+            IConnection connection;
+            try
+            {
+                connection = _idlePool.Dequeue();
+            }
+            catch (InvalidOperationException) //todo: disgusting!
+            {
+                connection = CreateNewConnection();                
+            }
             if (!string.IsNullOrEmpty(options))
             {
                 connection.LoadOptions(options);
