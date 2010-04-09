f8432b8f7e1dddc9c363b938902355ec967d63ff
averyj <javery@infozerk.com>
2010-04-09 22:24:16 +0000

    added set

---
+++ NoRM/Commands/Modifiers/SetOperation.cs
@@ -0,0 +1,19 @@
+﻿using NoRM.BSON;
+
+namespace NoRM.Commands
+{
+    /// <summary>
+    /// The increment operation.
+    /// </summary>
+    public class SetOperation : ModifierCommand
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IncrementOperation"/> class.
+        /// </summary>
+        /// <param name="value">The value to set.</param>
+        public SetOperation(object value)
+            : base("$set", value)
+        {
+        }
+    }
+}
