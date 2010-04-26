4f455fc9ee4bc3c7c3d4444788cc5000e39a92aa
Karl Seguin <karl@openmymind.net>
2010-04-26 11:39:15 -0400

    BaseStatusMessage now implements IExpando for a bit of future-proofing

--- NoRM/Protocol/SystemMessages/Responses/BaseStatusMessage.cs
+++ NoRM/Protocol/SystemMessages/Responses/BaseStatusMessage.cs
@@ -2,10 +2,13 @@
 
 namespace Norm.Responses
 {
+    using System;
+    using System.Collections.Generic;
+
     /// <summary>
     /// Represents a message with an Ok status
     /// </summary>
-    public class BaseStatusMessage
+    public class BaseStatusMessage : IExpando
     {
         static BaseStatusMessage()
         {
@@ -21,5 +24,15 @@ namespace Norm.Responses
         /// </summary>
         /// <value>The Ok property gets the Ok data member.</value>
         public double? Ok { get; set; }
+
+        private IDictionary<string, object> _expando;
+        public IDictionary<string, object> Expando
+        {
+            get
+            {
+                if (_expando == null) { _expando = new Dictionary<string, object>();}
+                return _expando;
+            }
+        }
     }
 }
