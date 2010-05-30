163459186a14a455ae9910f7b1da7aaf1abb580b
Andrew Theken <theken.1@osu.edu>
2010-05-30 19:19:08 -0400

    Removed and added a few files necessary for LINQ on the MongoCollection

---
+++ NoRM.Tests/Helpers/QueryTestHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Norm.Linq;
+
+namespace Norm.Tests.Helpers
+{
+    public static class QueryTestHelper
+    {
+        /// <summary>
+        /// Provides a way to pull the results of the query construction.. This is an implementation detail, 
+        /// but used to verify that our queries are appropriately optimized.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="queryable"></param>
+        /// <returns></returns>
+        public static QueryTranslationResults QueryStructure<T>(this IQueryable<T> queryable)
+        {
+             return ((IMongoQueryResults)queryable.Provider).TranslationResults;;
+        }
+
+    }
+}
