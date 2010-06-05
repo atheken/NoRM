19d8098a8cc1d390abef3f0a2c3cce84b61a16c7
Andrew Theken <theken.1@osu.edu>
2010-06-05 10:32:01 -0400

    Added start of the mongo configuration console.

---
+++ contrib/NorMan/NorMan/ConnectionDialog.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NorMan
+{
+    /// <summary>
+    /// Interaction logic for ConnectionDialog.xaml
+    /// </summary>
+    public partial class ConnectionDialog : Window
+    {
+        public ConnectionDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
